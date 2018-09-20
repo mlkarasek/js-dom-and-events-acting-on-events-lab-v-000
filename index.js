29de07abbb0001c7740e3a6ac89ea94d62a049e0
Monica  Karasek <mlkarasek@gmail.com>
2018-09-20 22:41:48 +0000

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,33 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+let input = document.querySelector('input')
+
+function retrieveEmployeeInformation(){
+  return input.value
+}
+
+function addNewElementAsLi(){
+  let employee = retrieveEmployeeInformation()
+  return document.querySelector('ul').append(employee)
+}
+
+function addNewLiOnClick(){
+  let employee = document.querySelector('input[type="submit"]')
+  employee.addEventListener('click', function(e){
+    addNewElementAsLi(e)
+    resetInput()
+  })
+}
+
+function resetInput(){
+   return document.querySelector('input').value = ''
+}
+
+function clearEmployeeListOnLinkClick(){
+  let clear = document.querySelector('a')
+  clear.addEventListener('click', function(e){
+    document.querySelector('ul').innerText = ''
+  })
+}
